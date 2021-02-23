a3ad3a19fd6bd10a9ff9bc28ce1fa9a5bb994fc3
eflipe <felipecabaleiro@gmail.com>
2021-02-23 13:18:53 -0300

    01 - models.py: definimos los modelos Language y Snippet

--- snippets/models.py
+++ snippets/models.py
@@ -4,8 +4,22 @@ from django.db import models
 
 
 class Language(models.Model):
-    pass
+    name = models.CharField(max_length=50, blank=False)
+    # slug blank=False
+
+    def __str__(self):
+        return self.name
 
 
 class Snippet(models.Model):
-    pass
+    # user = models.ForeignKey(Category, on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now_add=True)
+    name = models.CharField(max_length=255, blank=False)
+    description = models.TextField(blank=True)
+    snippet = models.TextField(blank=False)
+    language = models.ForeignKey(Language, on_delete=models.CASCADE)
+    public = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
